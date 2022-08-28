acdb768bb24e80d01ff6b1b805b4cac64afdc4d0
Stévan Corre <stevancorre@protonmail.com>
2022-08-28 18:49:44 +0200

    [configuration] add logger configuration

--- src/core/configuration.ts
+++ src/core/configuration.ts
@@ -2,6 +2,7 @@ import appConfig from "../../appconfig.json";
 
 export interface AppConfig {
     token: string;
+    logLevel: "error" | "warn" | "info" | "http" | "verbose" | "debug" | "silly";
 }
 
 export default appConfig as AppConfig;
