76c0e2b75497f887c9f25c20de6c4df4812f38de
Stévan Corre <stevancorre@protonmail.com>
2022-08-28 18:39:29 +0200

    [telegraf] simple setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Ok");
+import { Telegraf } from "telegraf";
+
+import appConfig from "./core/configuration";
+
+const bot = new Telegraf(appConfig.token);
+bot.launch();
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
