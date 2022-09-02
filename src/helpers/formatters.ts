2651b2a406ca5347ddda20a67681cee58ce6b6c9
Stévan Corre <stevancorre@protonmail.com>
2022-09-02 06:37:36 +0200

    [helpers] add formatters

---
+++ src/helpers/formatters.ts
@@ -0,0 +1,54 @@
+import { MinMax } from "../types/MinMax";
+import { Dictionary } from "./dictionary";
+import { StringBuilder } from "./stringBuilder";
+
+export function formatMinMax(
+    value: MinMax | number | null | undefined,
+    units: string | null | undefined,
+    unitAliases?: Dictionary<string, string>,
+): string {
+    if (!value || !units) {
+        return "";
+    }
+
+    if (unitAliases) {
+        units = unitAliases.tryGetValue(units, units);
+    }
+
+    // example: 40mg
+    if (typeof value === "number") {
+        return `${value}${units}`;
+    }
+
+    // example: 40mg - 50mg
+    // here, we handle cases were either `min` or `max` is undefined
+    const builder: StringBuilder = new StringBuilder();
+    if (value.min) {
+        builder.append(`${value.min}${units}`);
+        if (value.max) {
+            builder.append(` - ${value.max}${units}`);
+        }
+    } else if (value.max) {
+        builder.append(`${value.max}${units}`);
+    }
+
+    return builder.getContent();
+}
+
+export function capitalize(value: string): string {
+    return value[0].toUpperCase() + value.slice(1);
+}
+
+export function formatExternalLink(title: string, url: string): string {
+    title = capitalize(title.replace(/\(.*\)/gm, "").trim());
+    const websiteNameAndDomain: string = url
+        .replace(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n?]+)(.*)$/gim, "$1")
+        .trim()
+        .toLowerCase();
+
+    return `${title}: <a href="${url}">${websiteNameAndDomain}</a>`;
+}
+
+export function formatInt(n: number) {
+    return n.toFixed(0);
+}
