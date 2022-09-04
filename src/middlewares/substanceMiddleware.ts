0b989617de6c84bb9584237bdaad88eb58a2ef87
Stévan Corre <stevancorre@protonmail.com>
2022-09-04 18:25:23 +0200

    [middlewares/substance] implement

---
+++ src/middlewares/substanceMiddleware.ts
@@ -0,0 +1,24 @@
+import { Context } from "telegraf";
+
+import { replyToMessage } from "../helpers/telegraf";
+import { findSubstanceInTable } from "../tables/substancesAliases";
+
+export default async function substanceMiddleware(ctx: Context, next: () => Promise<void>) {
+    const message = ctx.message;
+    if (message === undefined) return;
+    if (!("text" in message)) return;
+
+    const command: string = message.text.split(" ")[0];
+    const source: string = message.text.substring(command.length).trim();
+
+    const match = /^(?<substance>.*\S.*)$/gim.exec(source);
+    if (match === null || match.groups === undefined)
+        return await ctx.replyWithMarkdownV2(
+            `Usage: \`${command} <substance>\`\nExample: \`${command} ketamine\``,
+            replyToMessage(ctx),
+        );
+
+    ctx.state.substance = findSubstanceInTable(match.groups.substance);
+
+    next();
+}
