dd454229c62fd090f4d5c10c54a6dc169e262662
Stévan Corre <stevancorre@protonmail.com>
2022-09-16 14:04:43 +0200

    [tables] add pretty substance names

---
+++ src/tables/prettySubstanceNames.ts
@@ -0,0 +1,34 @@
+export const substancesAliases: Record<string, string> = <const>{
+    lsd: "LSD",
+    mushrooms: "Mushrooms",
+    dmt: "DMT",
+    mescaline: "Mescaline",
+    dox: "DOx",
+    nbomes: "NBOMes",
+    "2c-x": "2C-x",
+    "2c-t-x": "2C-T-x",
+    "5-meo-xxt": "5-MEO-xxT",
+    cannabis: "Cannabis",
+    ketamine: "Ketamine",
+    mxe: "MXE",
+    dxm: "DXM",
+    nitrous: "Nitrous",
+    amphetamines: "Amphetamines",
+    mdma: "MDMA",
+    cocaine: "Cocaine",
+    caffeine: "Caffeine",
+    alcohol: "Alcohol",
+    "ghb/gbl": "GHB/GBL",
+    opioids: "Opioids",
+    tramadol: "Tramadol",
+    benzodiazepines: "Benzodiazepines",
+    benzos: "Benzodiazepines",
+    maois: "MAOIs",
+    ssris: "SSRIs",
+    pcp: "PCP",
+    amt: "AMT",
+};
+
+export const prettySubstanceName = (substance: string): string => {
+    return substancesAliases[substance] ?? substance;
+};
