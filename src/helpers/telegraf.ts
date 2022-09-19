af1866ef41f71207d372f0e01f62249f2d13198c
Stévan Corre <stevancorre@protonmail.com>
2022-09-19 21:34:14 +0200

    [helpers/telegraf] implement setMyCommands

--- src/helpers/telegraf.ts
+++ src/helpers/telegraf.ts
@@ -1,4 +1,7 @@
-import { Context } from "telegraf";
+import axios from "axios";
+import { Command } from "pwb/core/command";
+import appConfig from "pwb/core/configuration";
+import { Context, Telegraf } from "telegraf";
 import { Message } from "telegraf/typings/core/types/typegram";
 
 export const replyToMessage = (ctx: Context) => ({
@@ -19,4 +22,16 @@ Context.prototype.replyToMessageWithPhoto = async function (photo: string): Prom
 
 Context.prototype.replyToMessageWithVideo = async function (video: string): Promise<Message> {
     return await this.replyWithVideo(video, replyToMessage(this));
+};
+
+Telegraf.prototype.setMyCommands = async function (commands: ReadonlyArray<Command>): Promise<void> {
+    const data = {
+        commands: commands.map((x) => ({
+            command: x.name,
+            description: x.description,
+        })),
+    };
+
+    const url = `https://api.telegram.org/bot${appConfig.token}/setMyCommands`;
+    return axios.post(url, data);
 };
--- src/types/telegraf.d.ts
+++ src/types/telegraf.d.ts
@@ -1,6 +1,11 @@
+import { Command } from "pwb/core/command";
 import { Message } from "telegraf/typings/core/types/typegram";
 
 declare module "telegraf" {
+    interface Telegraf {
+        setMyCommands(commands: ReadonlyArray<Command>): Promise<void>;
+    }
+
     interface Context {
         replyToMessageWithErrorHTML(error: string): Promise<Message>;
         replyToMessageWithHTML(error: string): Promise<Message>;
