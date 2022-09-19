1ec564e18c4014da48b6babeec698b26cb23409f
Stévan Corre <stevancorre@protonmail.com>
2022-09-19 09:38:53 +0200

    [tables/aliases] force lowercase names

--- src/tables/substancesAliases.ts
+++ src/tables/substancesAliases.ts
@@ -3,11 +3,15 @@ export const substancesAliases: Record<string, string | string[]> = {
     "2c-b": ["2cb", "2c b"],
 };
 
-export const findSubstanceInTable = (substance: string): string | undefined =>
-    Object.entries(substancesAliases).find(([k, v]) => {
-        const substanceInV =
-            typeof v === "string"
-                ? v.toLowerCase() === substance
-                : v.find((x) => x.toLowerCase() === substance);
-        return k.toLowerCase() === substance || substanceInV;
-    })?.[0] ?? substance;
+export const findSubstanceInTable = (substance: string): string | undefined => {
+    substance = substance.toLowerCase();
+    return (
+        Object.entries(substancesAliases).find(([k, v]) => {
+            const substanceInV =
+                typeof v === "string"
+                    ? v.toLowerCase() === substance
+                    : v.find((x) => x.toLowerCase() === substance);
+            return k.toLowerCase() === substance || substanceInV;
+        })?.[0] ?? substance
+    );
+};
