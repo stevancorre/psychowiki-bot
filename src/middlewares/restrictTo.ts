563096111e46f4d4df3e97075d0c3a73cb6d166d
Stévan Corre <stevancorre@protonmail.com>
2022-09-20 15:45:22 +0200

    [middlewares/restrictTo] allow specific commands

--- src/middlewares/restrictTo.ts
+++ src/middlewares/restrictTo.ts
@@ -1,20 +1,33 @@
+import BugCommand from "pwb/commands/bug";
+import ChannelCommand from "pwb/commands/channel";
 import appConfig from "pwb/core/configuration";
 import { Context } from "telegraf";
+
+const ALLOWED_COMMANDS: ReadonlyArray<string> = <const>[ChannelCommand.name, BugCommand.name];
 
 /**
  * Allow command to be executed only in the one present in the configuration
  */
 export default async function restrictToMiddleware(ctx: Context, next: () => Promise<void>): Promise<void> {
     const chat = ctx.chat;
+    const message = ctx.message;
 
+    if (message === undefined) return;
+    if (!("text" in message)) return;
     if (chat?.id === undefined) return;
+
     if (appConfig.restrictTo) {
+        const commandWithMaybePing: string | undefined = message.text.split(" ")[0]?.toLowerCase();
+        const command: string | undefined = commandWithMaybePing?.split("@")[0]?.replace("/", "");
+        if (ALLOWED_COMMANDS.find((x) => x === command)) return next();
+
         if (chat.type === "private") {
             await ctx.replyToMessageWithErrorHTML(
                 "The bot isn't currently available in private.\nIt is currently in early alpha, and will be open to everyone soon",
             );
             return;
         }
+
         if (appConfig.restrictTo.find((x) => x === chat.id) === undefined) {
             await ctx.replyToMessageWithErrorHTML(
                 "The bot isn't currently available in this channel.\nIt is currently in early alpha, and will be open to everyone soon",
